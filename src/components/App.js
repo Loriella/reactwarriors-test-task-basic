8b32b2c3cf4d085b30aebf0140a490fa67e31d0e
Loriella <loris.orlova@gmail.com>
2019-12-10 18:49:22 +0300

    add loading

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ export class App extends React.Component {
     super(props);
 
     this.state = {
+      loading: false,
       galleryItems: [],
     }
   }
@@ -13,11 +14,16 @@ export class App extends React.Component {
   getGalleryData = () => {
     const link = "https://www.reddit.com/r/reactjs.json?limit=100";
 
+    this.setState({
+      loading: true
+    });
+
     fetch(link)
       .then(response => response.json())
       .then(({data}) => {
         this.setState({
-          galleryItems: data.children
+          galleryItems: data.children,
+          loading: false
         })
       })
   };
@@ -32,22 +38,25 @@ export class App extends React.Component {
     return (
       <div className="container">
         <h1>Top commented.</h1>
-        <div className="row mt-4">
-          {
-            galleryItems.map(item => {
-              return (
-                <div
-                  className="col-4"
-                  key={item.data.id}
-                >
-                  <GalleryItem
-                    data={item.data}
-                  />
-                </div>
-              )
-            })
-          }
-        </div>
+        {
+          this.state.loading
+            ? <div className="loader">Loading...</div>
+            : <div className="row mt-4">
+              {galleryItems.map(item => {
+                return (
+                  <div
+                    className="col-4"
+                    key={item.data.id}
+                  >
+                    <GalleryItem
+                      data={item.data}
+                    />
+                  </div>
+                )
+              })
+              }}
+            </div>
+        }
       </div>
     );
   }
