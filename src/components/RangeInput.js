fb3ced038d7c2eb5c2aa0d8823f4f64768d7b856
Loriella <loris.orlova@gmail.com>
2019-12-18 19:00:22 +0300

    add RangeInput component

---
+++ src/components/RangeInput.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class RangeInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0
+    }
+  }
+
+  onChange = event => {
+    const value = event.target.value;
+
+    this.setState({
+      value: value
+    });
+    this.props.onChangeRangeFilter(value)
+  };
+
+  render() {
+    return (
+      <form className="mt-3">
+        <div className="form-group">
+          <input
+            type="range"
+            className="form-control-range"
+            id="formControlRange"
+            min="0"
+            max="1540"
+            onChange={this.onChange}
+          />
+        </div>
+      </form>
+    )
+  }
+}
+
+export default RangeInput;
