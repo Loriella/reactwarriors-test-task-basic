84acd078716812d563bef8bc620badc795cb1ba8
Loriella <loris.orlova@gmail.com>
2019-12-18 20:20:21 +0300

    add comments filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GalleryItem from "./GalleryItem";
+import RangeInput from "./RangeInput";
 
 export class App extends React.Component {
   constructor(props) {
@@ -9,6 +10,7 @@ export class App extends React.Component {
       loading: false,
       galleryItems: [],
       refreshButton: false,
+      rangeFilter: 0
     }
   }
 
@@ -28,20 +30,20 @@ export class App extends React.Component {
       .then(({data}) => {
         this.setState({
           galleryItems: data.children.sort(function (a, b) {
-              if (a.data.num_comments < b.data.num_comments) {
-                return 1;
-              }
-              if (a.data.num_comments > b.data.num_comments) {
-                return -1;
-              }
-              return 0;
-            }),
+            if (a.data.num_comments < b.data.num_comments) {
+              return 1;
+            }
+            if (a.data.num_comments > b.data.num_comments) {
+              return -1;
+            }
+            return 0;
+          }),
           loading: false
         })
       })
   };
 
-  autoRefreshHandle = () => {
+  onChangeAutoRefresh = () => {
     this.setState(state => ({
         refreshButton: !state.refreshButton
       }), () => {
@@ -54,35 +56,48 @@ export class App extends React.Component {
     );
   };
 
+  onChangeRangeFilter = value => {
+    this.setState({
+      rangeFilter: value
+    })
+  };
+
   render() {
-    const {galleryItems, refreshButton} = this.state;
+    const {galleryItems, refreshButton, rangeFilter} = this.state;
+    const filteredGalleryItems = galleryItems.filter(item => item.data.num_comments >= Number(rangeFilter));
 
     return (
       <div className="container">
         <h1>Top commented.</h1>
+        <div className="filter-title">Current filter: {rangeFilter}</div>
         <button
           className="refresh-btn"
           type="button"
-          onClick={this.autoRefreshHandle}
+          onClick={this.onChangeAutoRefresh}
         >
           {refreshButton ? "Stop auto-refresh" : "Start auto-refresh"}
         </button>
+        <RangeInput
+          onChangeRangeFilter={this.onChangeRangeFilter}
+        />
         {
           this.state.loading
             ? <div className="loader">Loading...</div>
             : <div className="row mt-4">
-              {galleryItems.map(item => {
-                return (
-                  <div
-                    className="col-4"
-                    key={item.data.id}
-                  >
-                    <GalleryItem
-                      data={item.data}
-                    />
-                  </div>
-                )
-              })
+              { filteredGalleryItems.length === 0
+                ? <div className="message">No results found matching your criteria</div>
+                : filteredGalleryItems.map(item => {
+                  return (
+                    <div
+                      className="col-4"
+                      key={item.data.id}
+                    >
+                      <GalleryItem
+                        data={item.data}
+                      />
+                    </div>
+                  )
+                })
               }
             </div>
         }
